9838e03675d2f42ca7b161cf01e1bd5e7a3fa1ff
Deivsonbf <deivsonb7@gmail.com>
2021-12-10 11:56:10 -0300

    teste

--- server.js
+++ server.js
@@ -3,11 +3,8 @@ const port = process.env.PORT || 5000
 
 app.listen(port, (req, res) => {
     if (process.env.NODE_ENV == 'development') {
-<<<<<<< HEAD
         console.log(`${process.env.NODE_ENV}`);
-=======
         console.log(`http://localhost:${port}`);
->>>>>>> ac16a42caedf8cee27830bb701cc40dfa4ccb160
     } else {
         console.log(`${process.env.NODE_ENV}`);
     }
